874b8a574e1a7994d16b09df958a22e35dc81fd9
Marcin Kacik <marcinkacik@interia.pl>
2017-08-05 23:42:35 +0200

    Version 1.11, added ContactPicker

--- app/src/main/java/com/apps/szpansky/concat/add_edit/AddEditPersonActivity.java
+++ app/src/main/java/com/apps/szpansky/concat/add_edit/AddEditPersonActivity.java
@@ -1,9 +1,13 @@
 package com.apps.szpansky.concat.add_edit;
 
+import android.content.Intent;
 import android.database.Cursor;
+import android.net.Uri;
+import android.provider.ContactsContract;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -15,13 +19,15 @@ import com.google.android.gms.ads.AdView;
 
 public class AddEditPersonActivity extends AppCompatActivity {
 
+    private static final int RESULT_PICK_CONTACT = 12;
+
     AdView mAdView;
 
     private EditText name;
     private EditText surname;
     private EditText address;
     private EditText phone;
-    private FloatingActionButton add;
+    private FloatingActionButton add, openContacts;
 
     private Integer thisId = 0;
     private Boolean isEdit = false;
@@ -43,6 +49,7 @@ public class AddEditPersonActivity extends AppCompatActivity {
         address = (EditText) findViewById(R.id.add_edit_personAddress);
         phone = (EditText) findViewById(R.id.add_edit_personPhone);
         add = (FloatingActionButton) findViewById(R.id.add_edit_person_fab);
+        openContacts = (FloatingActionButton) findViewById(R.id.openContacts);
 
         if (bundle != null) {
 
@@ -58,6 +65,7 @@ public class AddEditPersonActivity extends AppCompatActivity {
             phone.setText(getPersonInfo(4));
         }
         onAddClick();
+        pickContact();
     }
 
 
@@ -116,5 +124,63 @@ public class AddEditPersonActivity extends AppCompatActivity {
         Cursor cursor = myDB.getRows(Database.TABLE_PERSONS, Database.PERSON_ID, thisId);
         cursor.moveToFirst();
         return cursor.getString(columnIndex);
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK) {
+            switch (requestCode) {
+                case RESULT_PICK_CONTACT:
+                    contactPicked(data);
+                    break;
+            }
+        } else {
+            Log.e("MainActivity", "Failed to pick contact");
+        }
+
+    }
+
+
+    public void contactPicked(Intent data) {
+        Cursor cursor = null;
+        try {
+            String dialerNo = null;
+            String dialerName = null;
+            Uri uri = data.getData();
+            cursor = getContentResolver().query(uri, null, null, null, null);
+            cursor.moveToFirst();
+            int phoneIndex = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER);
+            int nameIndex = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME);
+            dialerNo = cursor.getString(phoneIndex);
+            dialerName = cursor.getString(nameIndex);
+
+            String[] contactData = dialerName.split(" ");
+
+            if (contactData.length == 1) {
+                name.setText(contactData[0]);
+            }else if (contactData.length > 1) {
+                name.setText(contactData[0]);
+                surname.setText(contactData[1]);
+            }
+
+            phone.setText(dialerNo);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public void pickContact() {
+        openContacts.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent contactPickIntent = new Intent(Intent.ACTION_PICK,
+                        ContactsContract.CommonDataKinds.Phone.CONTENT_URI);
+                startActivityForResult(contactPickIntent, RESULT_PICK_CONTACT);
+            }
+        });
+
     }
 }
